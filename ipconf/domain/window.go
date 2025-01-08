bdae4de09a2c57bd36e5f96674fbd1a59ff58bd3
lxh-3260 <1906166914@qq.com>
2025-01-08 15:18:26 +0800

    ipconfig 滑动窗口解决流量毛刺

--- ipconf/domain/window.go
+++ ipconf/domain/window.go
@@ -1,7 +1,7 @@
 package domain
 
 const (
-	windowSize = 5
+	windowSize = 5 // 防止流量毛刺，取5个点的平均值
 )
 
 type stateWindow struct {
@@ -20,14 +20,16 @@ func newStateWindow() *stateWindow {
 }
 
 func (sw *stateWindow) getStat() *Stat {
-	res := sw.sumStat.Clone()
+	res := sw.sumStat.Clone() // 因为sumStat是指针，所以需要clone一份计算均值后返回
 	res.Avg(windowSize)
 	return res
 }
 
 func (sw *stateWindow) appendStat(s *Stat) {
-	// 减去即将被删除的state
-	sw.sumStat.Sub(sw.stateQueue[sw.idx%windowSize])
+	// 窗口大小没达到windowSize直接入窗，否则减去窗口最前面的state
+	if sw.idx >= windowSize {
+		sw.sumStat.Sub(sw.stateQueue[sw.idx%windowSize])
+	}
 	// 更新最新的stat
 	sw.stateQueue[sw.idx%windowSize] = s
 	// 计算最新的窗口和
